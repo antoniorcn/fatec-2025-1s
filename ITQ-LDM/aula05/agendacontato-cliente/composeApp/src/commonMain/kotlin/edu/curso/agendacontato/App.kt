0e871973cb438c103a58241a3a344ad29141e667
Antonio Carvalho <antoniorcn@hotmail.com>
2025-03-17 21:59:01 -0300

    ITQ-NLP Aula 05

--- ITQ-LDM/aula05/agendacontato-cliente/composeApp/src/commonMain/kotlin/edu/curso/agendacontato/App.kt
+++ ITQ-LDM/aula05/agendacontato-cliente/composeApp/src/commonMain/kotlin/edu/curso/agendacontato/App.kt
@@ -15,12 +15,16 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import agendacontato.composeapp.generated.resources.Res
 import agendacontato.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.TextButton
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -29,16 +33,24 @@ fun App() {
     Column( modifier = Modifier
         .fillMaxSize()
         .padding(30.dp)
-        .background(color = Color.Cyan, shape= RoundedCornerShape(20.dp))
+        .background(color = Color.Cyan)
         .padding(30.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally){
         Text("Ola Mundo Kotlin Multiplataforma")
         Text("Feito por Antonio Carvalho")
-        Button(onClick = {
+        Button(
+            modifier = Modifier,
+//                .background(color = Color.Yellow),
+            // shape = RectangleShape,
+            border = BorderStroke(0.dp, Color.Black),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0x33FFFF00)),
+            shape = RoundedCornerShape(10.dp),
+            onClick = {
             println("Botão foi clicado com sucesso")
         }) {
-            Text("Clique aqui")
+            Text(
+                "Clique aqui")
         }
     }
 }
