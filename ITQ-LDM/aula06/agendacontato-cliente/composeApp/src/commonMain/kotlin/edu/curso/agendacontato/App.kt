f41006d2b2c7751d22a6215bbde82e73992ddc8a
Antonio Carvalho <antoniorcn@hotmail.com>
2025-03-21 17:30:29 -0300

    ITQ-LDM Aula 06 parte 2

--- ITQ-LDM/aula06/agendacontato-cliente/composeApp/src/commonMain/kotlin/edu/curso/agendacontato/App.kt
+++ ITQ-LDM/aula06/agendacontato-cliente/composeApp/src/commonMain/kotlin/edu/curso/agendacontato/App.kt
@@ -1,18 +1,22 @@
 package edu.curso.agendacontato
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.MutableLongState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableLongStateOf
@@ -29,7 +33,16 @@ val email = mutableStateOf("")
 
 val contador : MutableLongState = mutableLongStateOf( 0 )
 
-val lista : MutableList<Contato> = mutableListOf()
+val tela : MutableState<String> = mutableStateOf("LIST")
+
+val lista : MutableList<Contato> = mutableListOf(
+    Contato(1, "Joao Silva", "joao@teste.com", "(11) 1111-1111"),
+    Contato(1, "Maria Silva", "maria@teste.com", "(11) 2222-2222"),
+    Contato(1, "Jose Silva", "jose@teste.com", "(11) 3333-3333")
+
+)
+
+val ciano = Color(0x77DDFFFF )
 
 fun adicionar() {
     println("Botão adicionar apertado=>")
@@ -45,37 +58,73 @@ fun adicionar() {
 @Composable
 @Preview
 fun App() {
-
-    val ciano = Color(0x77DDFFFF )
     MaterialTheme {
-        Column (verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .background(color= ciano)
-                .padding(vertical = 50.dp)
-                .fillMaxSize()
+        if (tela.value == "FORM") FormularioContato() else ListaContato()
+    }
+}
 
-            ) {
-            Text(text = "Agenda de Contato", fontSize = 48.sp)
-
-            OutlinedTextField(value = nome.value, onValueChange = { texto -> nome.value = texto },
-                label = {Text("Nome Completo")},
-                placeholder = { Text("Informe o nome completo do contato") },
-                modifier = Modifier.fillMaxWidth(0.8f))
-
-            OutlinedTextField(value = telefone.value, onValueChange = { texto -> telefone.value = texto },
-                label = {Text("Telefone")},
-                placeholder = { Text("Informe o telefone  (XX) XXXX-XXXX") },
-                modifier = Modifier.fillMaxWidth(0.8f))
-
-            OutlinedTextField(value = email.value, onValueChange = { email.value = it },
-                label = {Text("Email")},
-                placeholder = { Text("Informe um endereço de email válido") },
-                modifier = Modifier.fillMaxWidth(0.8f))
-
-            Button( onClick = { adicionar() } ) {
-                Text("Adicionar")
+@Composable
+fun ListaContato() {
+    Column (modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier
+            .fillMaxHeight(0.7f)
+            .fillMaxWidth()) {
+            items(lista) { contato ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding( 20.dp )
+                        .background(color = Color(0xFFFFFFCC), shape = RoundedCornerShape(16.dp) )
+                        .border(width = 2.dp, color=Color.Red, shape = RoundedCornerShape(16.dp))
+                        .padding( 10.dp )
+                ) {
+                    Text("Nome: ${contato.nome}")
+                    Text("Telefone: ${contato.telefone}")
+                    Text("Email: ${contato.email}")
+                }
             }
+        }
+        Column(modifier = Modifier
+            .fillMaxHeight(0.3f)) {
+            Button(onClick = { tela.value = "FORM" }) {
+                Text("Formulario")
+            }
+        }
+    }
+}
+
+@Composable
+fun FormularioContato() {
+    Column (verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(color= ciano)
+            .padding(vertical = 50.dp)
+            .fillMaxSize()
+
+    ) {
+        Text(text = "Agenda de Contato", fontSize = 48.sp)
+
+        OutlinedTextField(value = nome.value, onValueChange = { texto -> nome.value = texto },
+            label = {Text("Nome Completo")},
+            placeholder = { Text("Informe o nome completo do contato") },
+            modifier = Modifier.fillMaxWidth(0.8f))
+
+        OutlinedTextField(value = telefone.value, onValueChange = { texto -> telefone.value = texto },
+            label = {Text("Telefone")},
+            placeholder = { Text("Informe o telefone  (XX) XXXX-XXXX") },
+            modifier = Modifier.fillMaxWidth(0.8f))
+
+        OutlinedTextField(value = email.value, onValueChange = { email.value = it },
+            label = {Text("Email")},
+            placeholder = { Text("Informe um endereço de email válido") },
+            modifier = Modifier.fillMaxWidth(0.8f))
+
+        Button( onClick = { adicionar() } ) {
+            Text("Adicionar")
+        }
+        Button(onClick = { tela.value = "LIST" }) {
+            Text("Listagem")
         }
     }
 }
