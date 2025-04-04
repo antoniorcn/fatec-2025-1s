a78d10a87efe1d6c29764e1ffe390473e51569bb
Antonio Carvalho <antoniorcn@hotmail.com>
2025-04-03 22:04:51 -0300

    ZL-POO-Noite aula 08 parte 2

--- ZL-POO-NOITE/aula08/src/edu/curso/BorderPaneTeste.java
+++ ZL-POO-NOITE/aula08/src/edu/curso/BorderPaneTeste.java
@@ -4,30 +4,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-public class TesteApp extends Application { 
+public class BorderPaneTeste extends Application { 
 	@Override
 	public void start(Stage stage) {
-		Pane painel = new Pane();
+		BorderPane painel = new BorderPane();
 		Scene scn = new Scene( painel, 600, 400 );
 		Label texto = new Label("Ola mundo JavaFX");
 		Button btn = new Button("Clique aqui");
 		TextField txtNome = new TextField();
 
-		texto.relocate(300, 50);
-		txtNome.relocate(300, 150);
-		txtNome.setPrefSize(300, 30);
-		btn.relocate(300, 250);
-
-		// painel.getChildren().add( texto );
-		// painel.getChildren().add( btn );
-		painel.getChildren().addAll( texto, btn, txtNome );
+		painel.setTop(texto);
+		painel.setCenter(txtNome);
+		painel.setBottom( btn );
 		stage.setScene( scn );
 		stage.setTitle("Ola Mundo Java FX");
 		stage.show();
 	}
 	public static void main(String[] args) { 
-		Application.launch(TesteApp.class, args);
+		Application.launch(BorderPaneTeste.class, args);
 	}
 }
--- ZL-POO-NOITE/aula08/src/edu/curso/GridPaneTeste.java
+++ ZL-POO-NOITE/aula08/src/edu/curso/GridPaneTeste.java
@@ -4,30 +4,32 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
-public class TesteApp extends Application { 
+public class GridPaneTeste extends Application { 
 	@Override
 	public void start(Stage stage) {
-		Pane painel = new Pane();
+		GridPane painel = new GridPane();
 		Scene scn = new Scene( painel, 600, 400 );
 		Label texto = new Label("Ola mundo JavaFX");
 		Button btn = new Button("Clique aqui");
 		TextField txtNome = new TextField();
 
-		texto.relocate(300, 50);
-		txtNome.relocate(300, 150);
-		txtNome.setPrefSize(300, 30);
-		btn.relocate(300, 250);
+		painel.setHgap(20);
+		painel.setVgap(50);
 
-		// painel.getChildren().add( texto );
-		// painel.getChildren().add( btn );
-		painel.getChildren().addAll( texto, btn, txtNome );
+		painel.add( texto, 0, 0, 2, 1 );
+		painel.add( btn, 0, 1 );
+		painel.add(txtNome, 1, 1, 3, 1);
+		
 		stage.setScene( scn );
 		stage.setTitle("Ola Mundo Java FX");
 		stage.show();
 	}
 	public static void main(String[] args) { 
-		Application.launch(TesteApp.class, args);
+		Application.launch(GridPaneTeste.class, args);
 	}
 }
