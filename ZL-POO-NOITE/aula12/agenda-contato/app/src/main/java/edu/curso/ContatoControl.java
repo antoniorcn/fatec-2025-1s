2832cc1d25a3af3fc42075dec6f392586f130ee1
Antonio Carvalho <antoniorcn@hotmail.com>
2025-05-08 21:00:37 -0300

    ZL-POO-NOITE aula 12 parte 1

---
+++ ZL-POO-NOITE/aula12/agenda-contato/app/src/main/java/edu/curso/ContatoControl.java
@@ -0,0 +1,21 @@
+package edu.curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContatoControl {
+    private List<Contato> lista = new ArrayList<>();
+
+    public void adicionar( Contato c) {
+        lista.add( c );
+    }
+
+    public Contato pesquisarPorNome( String nome ){ 
+        for (Contato c : lista) { 
+            if (c.getNome().contains( nome )) { 
+                return c;
+            }
+        }
+        return null;
+    }
+}
